6e2e26945bd2ac85e59603ed51fe377f423535d4
Evan ROGIER <evan.rogier@epfedu.fr>
2023-09-25 20:14:27 +0200

    Partie 1,2 et 3 - TP0 Travail fini

--- TP0_ROGIER/Calculator/src/main/java/com/mycompany/calculator/Calculator.java
+++ TP0_ROGIER/Calculator/src/main/java/com/mycompany/calculator/Calculator.java
@@ -17,15 +17,38 @@ public class Calculator {
 
     public static void main(String[] args) {
         System.out.println("Please enter the operator: \n1) add \n2) substract \n3) multiply \n3) divide \n3) modulo");
-        int nb1; // nombre dentiers a additionner
+        int nb;
+        int nb1;
+        int nb2;
+        double resultat = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("\n Entrer le nombre :");
-        nb1 = sc.nextInt(); // On demande a sc de donner le prochain entier
-        int operateur = nb1;
-        int nb3; // nombre dentiers a additionner
-        Scanner sc = new Scanner(System.in);
-        System.out.println("\n Entrer le nombre :");
-        nb2 = sc.nextInt(); // On demande a sc de donner le prochain entier
-        int operateur = nb2;
+        nb = sc.nextInt(); // On demande a sc de donner le prochain entier
+        int operateur = nb;
+        System.out.println("\n Entrer le nombre 1 :");
+        nb1 = sc.nextInt();
+        int operande1 = nb1;
+        System.out.println("\n Entrer le nombre 2 :");
+        nb2 = sc.nextInt();
+        int operande2 = nb2;
+        
+        if (operateur == 1) {
+            resultat = operande1 + operande2;
+       
+        } else if (operateur == 2) {      
+            resultat = operande1 - operande2;
+       
+        } else if (operateur == 3) {
+            resultat = operande1 * operande2;
+       
+        } else if (operateur == 4) {
+            resultat = operande1 / operande2;
+       
+        } else if (operateur == 5) {
+            resultat = operande1 % operande2;
+            
+        } else if (operateur <1 & operateur >5) {
+            System.out.println("\n Erreur, cela doit être compris entre 1 et 5");
+        }System.out.println("le résultat est :"+resultat);
     }
 }
