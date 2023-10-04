b05040b70b2278144296aa22116d2b7ac0cf8f6f
Evan ROGIER <evan.rogier@epfedu.fr>
2023-10-04 15:24:23 +0200

    2_TP1 finie

---
+++ TP1_ROGIER/Exo2/src/main/java/com/mycompany/exo2/TP1_convertisseur_ROGIER.java
@@ -0,0 +1,25 @@
+/*
+ROGIER Evan
+TDC
+TP1
+26/09/2023
+    */
+
+package com.mycompany.exo2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Evan1204
+ */
+public class TP1_convertisseur_ROGIER {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //même base que dans les TP0 et TP1
+        System.out.println("\n Saisissez uen valeur réel :");
+        double nb = sc.nextDouble(); // utilisation du type Double
+        double valeur = nb;
+        System.out.println(valeur);
+    }
+}
