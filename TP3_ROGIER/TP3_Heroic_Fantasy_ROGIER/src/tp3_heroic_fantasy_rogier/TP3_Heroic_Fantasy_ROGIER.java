3b36fd9ae56600b84fc9789be7c9f4c06046dbf9
Evan ROGIER <evan.rogier@epfedu.fr>
2023-10-18 15:30:36 +0200

    TP3 question 12 en cours

---
+++ TP3_ROGIER/TP3_Heroic_Fantasy_ROGIER/src/tp3_heroic_fantasy_rogier/TP3_Heroic_Fantasy_ROGIER.java
@@ -0,0 +1,26 @@
+/*
+ROGIER Evan
+TDC
+TP2
+18/10/2023
+ */
+package tp3_heroic_fantasy_rogier;
+
+/**
+ *
+ * @author Evan1204
+ */
+public class TP3_Heroic_Fantasy_ROGIER {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Epee Excalibur = new Epee("Excalibur",7,5);
+        Epee Durandal = new Epee("Durandal",4,7);
+        
+        Baton Chêne = new Baton("Chêne", 4, 5);
+        Baton Charme = new Baton("Charme", 5, 6);
+    }
+    
+}
